5a8f0e22e77aec281cab0e21560c0ffc442e266b
Surabhi Jadhav <jadhavsu@mediamarktsaturn.com>
2023-07-20 14:54:17 +0200

    Kotlin Refractoring techSummit Challenge

--- 5_Kotlin_Refactoring/src/main/kotlin/com/mediamarktsaturn/techsummit23/Main.kt
+++ 5_Kotlin_Refactoring/src/main/kotlin/com/mediamarktsaturn/techsummit23/Main.kt
@@ -1,7 +1,11 @@
 package com.mediamarktsaturn.techsummit23
 
 import org.apache.hc.core5.http.HttpResponse
-import java.io.*
+import java.io.ByteArrayOutputStream
+import java.io.ObjectOutputStream
+import java.io.IOException
+import java.io.Serializable
+import java.io.File
 import java.util.Scanner
 
 fun Serializable.toByteArray(): ByteArray {
@@ -14,37 +18,57 @@ fun Serializable.toByteArray(): ByteArray {
         val result = byteArrayOutputStream.toByteArray()
         return result
     } finally {
-        if (objectOutputStream != null) {
-            objectOutputStream.close()
-        }
+        //Old Code
+        /* if (objectOutputStream != null) {
+             objectOutputStream.close()
+         }*/
+
+        //Refractored code
+        objectOutputStream?.let { objectOutputStream.close() }
         byteArrayOutputStream.close()
     }
 }
 
 fun handleResponse(response: HttpResponse?): String {
-    if (response == null) {
-        throw IllegalStateException("Response is null")
+    //Old code
+    /* if (response == null) {
+       throw IllegalStateException("Response is null")
+   }*/
+
+    //Refractored code
+    val responseNotNull = response ?: throw IllegalStateException("Response is null")
+    val code: Int = responseNotNull.code
+    when (responseNotNull.code) {
+        null -> throw IllegalStateException("Response is null")
+        200, 201 -> return response.reasonPhrase
+        in 400..499 -> throw IllegalStateException("Server responses with client error")
+        in 500..599 -> throw IllegalStateException("Server responses with server error")
+        else -> {
+            throw IllegalStateException("Receive unexpected response code " + code)
+        }
     }
 
-    val code: Int = response.code
-    if (code == 200 || code == 201) {
-        return response.reasonPhrase
-    }
+    //Old code
+    /* val code: Int = response.code
+     if (code == 200 || code == 201) {
+         return response.reasonPhrase
+     }
 
-    if (code >= 400 && code <= 499) {
-        throw IllegalStateException("Server responses with client error")
-    }
+     if (code >= 400 && code <= 499) {
+         throw IllegalStateException("Server responses with client error")
+     }
 
-    if (code >= 500 && code <= 599) {
-        throw IllegalStateException("Server responses with server error")
-    }
+     if (code >= 500 && code <= 599) {
+         throw IllegalStateException("Server responses with server error")
+     }
 
-    throw IllegalStateException("Receive unexpected response code " + code)
+     throw IllegalStateException("Receive unexpected response code " + code)*/
 }
 
 private const val DELIMITER = "[^a-zA-Z'äöü]+"
 
 fun File.countWord(): MutableMap<String, Int> {
+    val scan = Scanner(System.`in`)
     var scanner: Scanner? = null
     try {
         scanner = Scanner(this)
@@ -52,21 +76,41 @@ fun File.countWord(): MutableMap<String, Int> {
         val wordCount: MutableMap<String, Int> = mutableMapOf<String, Int>()
         while (scanner.hasNext()) {
             val word: String = scanner.next()
-            if (!wordCount.containsKey(word)) {
-                wordCount.put(word, 1)
-            } else {
-                val currentCount = wordCount.get(word)
-                if (currentCount != null) {
-                    wordCount.put(word, currentCount + 1)
+
+            //Refractored code
+            val wordCountBoolean: Boolean = wordCount.containsKey(word)
+            when (wordCountBoolean) {
+                false -> wordCount.put(word, 1)
+                true -> {
+                    val currentCount = wordCount.get(word)
+                    currentCount?.let { wordCount.put(word, currentCount + 1) }
                 }
+                //Old code
+               /* if (!wordCount.containsKey(word)) {
+                    wordCount.put(word, 1)
+
+                } else {
+                    val currentCount = wordCount.get(word)
+
+
+                    if (currentCount != null) {
+                        wordCount.put(word, currentCount + 1)
+                    }
+
+                    currentCount?.let { wordCount.put(word, currentCount + 1) }
+                }*/
             }
         }
         return wordCount
     } catch (e: IOException) {
         throw IllegalArgumentException("Unable to read the current file ${this.name}!", e)
     } finally {
-        if (scanner != null) {
-            scanner.close()
-        }
+        //Old Code
+        /* if (scanner != null) {
+             scanner.close()
+         }*/
+
+        //Refractored code
+        scanner?.let { scanner.close() }
     }
 }
